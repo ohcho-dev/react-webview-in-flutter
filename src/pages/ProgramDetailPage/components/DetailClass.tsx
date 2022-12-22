9d48d71c91e6c32ad70e7039d489e33cdff8850d
ohcho-dev <ohcho.dev@gmail.com>
2022-12-22 14:34:39 +0900

    chore: 클래스 신청하기 버튼 눌렀을때 기능 구현

--- src/pages/ProgramDetailPage/components/DetailClass.tsx
+++ src/pages/ProgramDetailPage/components/DetailClass.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import ProgramPrice from '../../../components/ProgramPrice';
+import CustomModal from '../../../components/common/CustomModal';
+import ProgramPrice from '../../ProgramPage/components/ProgramPrice';
+import { AgeRange, OnlineOffline } from '../../ProgramPage/components/styled';
 
 interface DetailClassProps {
   id: string;
+  isApplyBtnClick: boolean;
+  setApplyBtnState: () => void;
 }
 
 const ClassWrapper = styled.div`
@@ -25,26 +30,7 @@ const ClassInfoWrapper = styled.div`
 const ClassInfo = styled.div`
   display: flex;
   margin: 0 0 1rem 0;
-  span {
-    font-weight: 700;
-    font-size: 1.6rem;
-    color: rgba(10, 10, 10, 0.5);
-    margin-right: 0.5rem;
-  }
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    font-weight: 600;
-    font-size: 1.2rem;
-    color: rgba(10, 10, 10, 0.5);
-
-    height: 1.8rem;
-    padding: 0px 4px;
-    border: 1px solid rgba(10, 10, 10, 0.5);
-    border-radius: 0.2rem;
-  }
+  align-items: center;
 `;
 
 const ClassTitle = styled.div`
@@ -66,9 +52,10 @@ const Divider = styled.div`
 `;
 
 const DetailClass: React.FC<DetailClassProps> = props => {
-  const { id } = props;
+  const navigate = useNavigate();
+  const { id, isApplyBtnClick, setApplyBtnState } = props;
   const [classInfo, setClassInfo] = useState({
-    isOnline: false,
+    isOnline: true,
     ageRange: '12~15개월',
     title: '[모집10명] 아빠랑 같이 하는 모래놀이 클래스',
     location: '서울 송파구 어린이 문화회관',
@@ -78,28 +65,53 @@ const DetailClass: React.FC<DetailClassProps> = props => {
     perNum: '1회당',
     dateTime: '2022.11.22(화) 21:00',
   });
+  const [openInformForPaymentModal, setOpenInformForPaymentModal] = useState(false);
+
+  const toggleInformPaymentModal = () => {
+    setOpenInformForPaymentModal(!openInformForPaymentModal);
+  };
+
+  useEffect(() => {
+    if (!classInfo.isOnline && isApplyBtnClick) {
+      toggleInformPaymentModal();
+    } else if (classInfo.isOnline && isApplyBtnClick) {
+      navigate(`/program/class/apply-class/${id}`);
+    }
+  }, [isApplyBtnClick]);
+
+  useEffect(() => {
+    if (!openInformForPaymentModal) setApplyBtnState();
+  }, [openInformForPaymentModal]);
 
   return (
-    <ClassWrapper>
-      <img alt="class image" src="/images/class-img.png" />
-      <ClassInfoWrapper>
-        <ClassInfo>
-          <span>{classInfo.isOnline ? '온라인' : '오프라인'}</span>
-          <div>{classInfo.ageRange}</div>
-        </ClassInfo>
-        <ClassTitle>{classInfo.title}</ClassTitle>
-        <ClassSubSection>
-          {classInfo.isOnline ? classInfo.dateTime : classInfo.location}
-        </ClassSubSection>
-        <ProgramPrice
-          price={classInfo.price}
-          discountPercentage={classInfo.discountPercentage}
-          originalPrice={classInfo.originalPrice}
-          perNum={'1회당'}
-        />
-      </ClassInfoWrapper>
-      <Divider />
-    </ClassWrapper>
+    <>
+      <ClassWrapper>
+        <img alt="class image" src="/images/class-img.png" />
+        <ClassInfoWrapper>
+          <ClassInfo>
+            <OnlineOffline>{classInfo.isOnline ? '온라인' : '오프라인'}</OnlineOffline>
+            <AgeRange>{classInfo.ageRange}</AgeRange>
+          </ClassInfo>
+          <ClassTitle>{classInfo.title}</ClassTitle>
+          <ClassSubSection>
+            {classInfo.isOnline ? classInfo.dateTime : classInfo.location}
+          </ClassSubSection>
+          <ProgramPrice
+            price={classInfo.price}
+            discountPercentage={classInfo.discountPercentage}
+            originalPrice={classInfo.originalPrice}
+            perNum={'1회당'}
+          />
+        </ClassInfoWrapper>
+        <Divider />
+      </ClassWrapper>
+      <CustomModal
+        isOpen={openInformForPaymentModal}
+        toggleModal={toggleInformPaymentModal}
+        title="현장 결제 프로그램입니다."
+        content="신청 시 예약만 진행됩니다."
+      />
+    </>
   );
 };
 
