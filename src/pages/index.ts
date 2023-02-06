eff34693c7a46cd8f5bc4701d2290c931c8c8eab
ohcho-dev <ohcho.dev@gmail.com>
2023-02-06 17:09:33 +0900

    chore: 필요없는 파일 삭제

--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import HomePage from "./HomePage";
-import CoachingPage from "./CoachingPage";
-import ProgramPage from "./ProgramPage";
-import ProgramDetailPage from "./ProgramDetailPage";
-import NotePage from "./NotePage";
-import MyPage from "./Mypage";
-
-export { HomePage, NotePage, CoachingPage, ProgramPage, ProgramDetailPage, MyPage };
