d0244af164620c21a64a11f9cfcd22910c4f4640
ohcho-dev <ohcho.dev@gmail.com>
2023-02-10 13:28:47 +0900

    fix: 라이브러리 변경

--- src/utils/RouteChangeTracker.tsx
+++ src/utils/RouteChangeTracker.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 // connect react-ga
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID || "";
 
@@ -8,14 +8,19 @@ const RouteChangeTracker = () => {
   const location = useLocation();
   const [initialized, setInitialized] = useState(false);
 
+  // localhost는 기록하지 않음
   useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-    setInitialized(true);
+    if (!window.location.href.includes("localhost")) {
+      ReactGA.initialize(TRACKING_ID);
+      setInitialized(true);
+    }
   }, []);
 
+  // location 변경 감지시 pageview 이벤트 전송
   useEffect(() => {
     if (initialized) {
-      ReactGA.pageview(location.pathname + location.search);
+      ReactGA.set({ page: location.pathname + location.search });
+      ReactGA.send("pageview");
     }
   }, [initialized, location]);
 };
