3d82d7b83d8476532550d87a6fa5e63bff8bb172
ohcho-dev <ohcho.dev@gmail.com>
2023-03-17 14:30:37 +0900

    fix: naver blog iframe test

--- src/pages/HomePage/ActivityDetail.tsx
+++ src/pages/HomePage/ActivityDetail.tsx
@@ -11,30 +11,56 @@ import LayoutDetailPage from "../../layouts/LayoutDetailPage";
 
 import { mainPageScrollValueState } from "../../recoil/atom";
 import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+
+const IframeWrap = styled.div`
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
 
 function ActivityDetail() {
   const navigate = useNavigate();
   const { state } = useLocation();
   const [data, setData] = useState({});
+  const [newUrl, setNewUrl] = useState("");
   const scroll = useRecoilValue(mainPageScrollValueState);
 
   useEffect(() => {
-    fetch(`https://notion-api.splitbee.io/v1/page/${state.substr(37)}`)
-      .then(res => res.json())
-      .then(data => setData(data));
+    if (state.indexOf("notion") !== -1) {
+      fetch(`https://notion-api.splitbee.io/v1/page/${state.substr(37)}`)
+        .then(res => res.json())
+        .then(data => setData(data));
+    } else {
+      state.indexOf("m.blog") && setNewUrl(state.replace("blog", "m.blog"));
+    }
   }, [state]);
 
   return (
-    <LayoutDetailPage
-      titleBarBorder
-      handleBackBtnClick={() => navigate("/home", { state: scroll })}
-    >
-      {data.constructor === Object && Object.keys(data).length === 0 ? (
-        <LoadingSpinner />
-      ) : (
-        <NotionRenderer blockMap={data} fullPage={true} />
+    <>
+      {newUrl && (
+        <IframeWrap>
+          <Iframe frameBorder={0} src={newUrl}></Iframe>
+        </IframeWrap>
       )}
-    </LayoutDetailPage>
+
+      {data && (
+        <LayoutDetailPage
+          titleBarBorder
+          handleBackBtnClick={() => navigate("/home", { state: scroll })}
+        >
+          <NotionRenderer blockMap={data} fullPage={true} />
+        </LayoutDetailPage>
+      )}
+    </>
   );
 }
 
