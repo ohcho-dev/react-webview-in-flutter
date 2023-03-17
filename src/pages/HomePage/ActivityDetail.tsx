a342a0d2bee273e187b913f4f2e2ebbd9f536a6c
ohcho-dev <ohcho.dev@gmail.com>
2023-03-17 14:45:01 +0900

    fix: home 콘텐츠 주소값에 따른 분기처리

--- src/pages/HomePage/ActivityDetail.tsx
+++ src/pages/HomePage/ActivityDetail.tsx
@@ -11,56 +11,30 @@ import LayoutDetailPage from "../../layouts/LayoutDetailPage";
 
 import { mainPageScrollValueState } from "../../recoil/atom";
 import { useRecoilValue } from "recoil";
-import styled from "styled-components";
-
-const IframeWrap = styled.div`
-  position: relative;
-  display: block;
-  width: 100%;
-  height: 100%;
-`;
-const Iframe = styled.iframe`
-  width: 100%;
-  height: 100vh;
-  position: absolute;
-  top: 0;
-  left: 0;
-`;
 
 function ActivityDetail() {
   const navigate = useNavigate();
   const { state } = useLocation();
   const [data, setData] = useState({});
-  const [newUrl, setNewUrl] = useState("");
   const scroll = useRecoilValue(mainPageScrollValueState);
 
   useEffect(() => {
-    if (state.indexOf("notion") !== -1) {
-      fetch(`https://notion-api.splitbee.io/v1/page/${state.substr(37)}`)
-        .then(res => res.json())
-        .then(data => setData(data));
-    } else {
-      state.indexOf("m.blog") && setNewUrl(state.replace("blog", "m.blog"));
-    }
+    fetch(`https://notion-api.splitbee.io/v1/page/${state.substr(37)}`)
+      .then(res => res.json())
+      .then(data => setData(data));
   }, [state]);
 
   return (
-    <>
-      {newUrl && (
-        <IframeWrap>
-          <Iframe frameBorder={0} src={newUrl}></Iframe>
-        </IframeWrap>
+    <LayoutDetailPage
+      titleBarBorder
+      handleBackBtnClick={() => navigate("/home", { state: scroll })}
+    >
+      {data.constructor === Object && Object.keys(data).length === 0 ? (
+        <LoadingSpinner />
+      ) : (
+        <NotionRenderer blockMap={data} fullPage={true} />
       )}
-
-      {data && (
-        <LayoutDetailPage
-          titleBarBorder
-          handleBackBtnClick={() => navigate("/home", { state: scroll })}
-        >
-          <NotionRenderer blockMap={data} fullPage={true} />
-        </LayoutDetailPage>
-      )}
-    </>
+    </LayoutDetailPage>
   );
 }
 
