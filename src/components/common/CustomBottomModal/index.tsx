6d9cce6bb31c715014c97d6f67ba240dbf87e128
ohcho-dev <ohcho.dev@gmail.com>
2023-06-26 14:14:18 +0900

    feat: change custom bottom sheet modal logic

--- src/components/common/CustomBottomModal/index.tsx
+++ src/components/common/CustomBottomModal/index.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
+import { isAndroid } from "react-device-detect";
+import ReactDOM from "react-dom";
+import { useNavigate } from "react-router-dom";
 import * as S from "./CustomBottomModal.styled";
 
 interface CustomBottomModalProps {
@@ -6,24 +9,38 @@ interface CustomBottomModalProps {
   handleToggle: () => void;
   children?: ReactNode;
 }
+
+const BottomSheetModalPortal = ({ children }: { children: ReactNode }) => {
+  const el = document.getElementById("body");
+  return el && ReactDOM.createPortal(children, el);
+};
+
 const CustomBottomModal: React.FC<CustomBottomModalProps> = ({
   toggle,
   handleToggle,
   children,
 }) => {
   // 뒤로가기 제어
+
+  const navigate = useNavigate();
+  const popStateRef = useRef<boolean>(false);
   useEffect(() => {
     // 개발 환경에서는 strict mode로 인해 렌더링이 2번 진행됨에 따라 뒤로가기를 2번 해야함.
     // 배포 환경에서는 이상 없음.
+    popStateRef.current = false;
     if (toggle) {
       document.getElementById("main")?.classList.add("scroll_lock");
-      window.history.pushState(null, "", window.location.href); // 현재 페이지 history stack 한개 더 쌓기
+      if (isAndroid) window.history.pushState(null, "", window.location.href);
       window.onpopstate = () => {
         // 뒤로가기가 실행될 경우 추가 action 등록
+        popStateRef.current = true;
         handleToggle();
       };
       return () => {
         document.getElementById("main")?.classList.remove("scroll_lock");
+        if (isAndroid && !popStateRef.current) {
+          navigate(-1);
+        }
       };
     }
   }, [toggle]);
@@ -31,10 +48,12 @@ const CustomBottomModal: React.FC<CustomBottomModalProps> = ({
   return (
     <>
       {toggle && (
-        <S.BottomModalWrap>
-          <S.Dimmed onClick={handleToggle} />
-          <S.Modal className="fadein-moveTop">{children}</S.Modal>
-        </S.BottomModalWrap>
+        <BottomSheetModalPortal>
+          <S.BottomModalWrap>
+            <S.Dimmed onClick={() => handleToggle()} />
+            <S.Modal className="fadein-moveTop">{children}</S.Modal>
+          </S.BottomModalWrap>
+        </BottomSheetModalPortal>
       )}
     </>
   );
