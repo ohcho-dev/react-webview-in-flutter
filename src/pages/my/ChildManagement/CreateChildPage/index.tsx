c20fc38885f1e0b213316cfcba2bebd85bfb5f5b
ohcho-dev <ohcho.dev@gmail.com>
2023-06-27 16:28:57 +0900

    feat: refactor create child page

--- src/pages/my/ChildManagement/CreateChildPage/index.tsx
+++ src/pages/my/ChildManagement/CreateChildPage/index.tsx
@@ -3,7 +3,6 @@ import dayjs from "dayjs";
 import React, { useEffect, useRef, useState, forwardRef } from "react";
 import DatePicker from "react-datepicker";
 import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -13,59 +12,69 @@ import { CustomRadioButton } from "../../../../components/common/CustomRadioButt
 import LayoutDetailPage from "../../../../layouts/LayoutDetailPage";
 import { ForwardedInput } from "../../../../components/common/DatePickerInput";
 import PageTitle from "../../../../components/domain/my/PageTitle";
-import { createChildType, OptionType } from "../../../../types/domain/my";
 import useCreateChild from "../../../../queries/domain/my/child/useCreateChild";
-import { childrenListState } from "store/common";
 import * as S from "../childManagement.styled";
 import AffiliatedOrganizationBox from "components/domain/my/AffiliatedOrganizationBox";
+import ConfirmAffiliateOrganizationStatusModal from "components/domain/my/ConfirmAffiliateOrganizationStatusModal.tsx";
+import ConfirmDeleteOrganizationModal from "components/domain/my/ConfirmDeleteOrganizationModal";
+import RejectDeleteOrganizationModal from "components/domain/my/RejectDeleteOrganizationModal";
+import RejectChangeOrganizationModal from "components/domain/my/RejectChangeOrganizationModal";
+import useValidChildInfo from "hooks/my/useValidChildInfo";
+import { OptionType } from "types/common";
+import { CreateChildObjType } from "types/domain/my";
+import { DEFAULT_CREATE_CHILD_VALUE } from "utils/default";
 
-export const DEFAULT_CHILD_TYPE = {
-  id: 0,
-  name: "",
-  gender: "",
-  birth_date: dayjs(new Date()).format("YYYY-MM-DD"),
-  premature_flag: 0,
-  due_date: "",
-  image: "",
-  parent_id: 0,
-  birth_modifiable: false,
-};
-
-const Genders: OptionType[] = [
+const GenderOption: OptionType[] = [
   { name: "여아", value: "F" },
   { name: "남아", value: "M" },
 ];
-const Prematures: OptionType[] = [
+const PrematureOption: OptionType[] = [
   { name: "예정일 출산", value: 0 },
   { name: "이른둥이 출산", value: 1 },
 ];
 
-const DEFAULT_GENDER = { name: "여아", value: "F" };
-const DEFAULT_PREMATURE = { name: "예정일 출산", value: 0 };
+const validationModalTitleArr = [
+  "아이는 최대 5명 이상 등록할 수 없습니다.",
+  "아이 이름을 입력해주세요.",
+  "같은 이름의 아이를 등록할 수 없습니다.",
+];
 
 const CreateChildPage = () => {
   const navigate = useNavigate();
-  const [childData, setChildData] = useState<createChildType>(DEFAULT_CHILD_TYPE);
+  const [childData, setChildData] = useState<CreateChildObjType>(DEFAULT_CREATE_CHILD_VALUE);
   const [birthDate, setBirthDate] = useState<Date | null>(new Date());
   const [dueDate, setDueDate] = useState<Date | null>(new Date());
-  const [openLengthModal, setOpenLengthModal] = useState(false);
-  const [openNameCheckModal, setOpenNameCheckModal] = useState(false);
-  const [openSameNameCheckModal, setOpenSameNameCheckModal] = useState(false);
+  const [validationModalTitle, setValidationModalTitle] = useState<string>("");
+  const [openValidationModal, setOpenValidationModal] = useState<boolean>(false);
   const [openSaveModal, setOpenSaveModal] = useState(false);
   const inputRef = useRef(null);
-  const childrenList = useRecoilValue(childrenListState);
+  const [openRejectDeleteOrganizationModal, setOpenRejectDeleteOrganizationModal] = useState(false);
+  const [openRejectChangeOrganizationModal, setOpenRejectChangeOrganizationModal] = useState(false);
+  const [openConfirmDeleteOrganizationModal, setOpenConfirmDeleteOrganizationModal] =
+    useState(false);
+  const [openAffiliatedConfirmModal, setOpenAffiliatedConfirmModal] = useState(false);
   const { mutate: createChild } = useCreateChild(setOpenSaveModal);
+  const [isValid, titleNum] = useValidChildInfo(childData);
 
   //   생일 날짜 string으로 변환
   useEffect(() => {
     setChildData({ ...childData, birth_date: dayjs(birthDate).format("YYYY-MM-DD") });
-    setDueDate(birthDate);
+    setDueDate(dayjs(birthDate).add(1, "day").toDate());
   }, [birthDate]);
 
   // 이른둥이 출산일 날짜 string으로 변환
   useEffect(() => {
     setChildData({ ...childData, due_date: dayjs(dueDate).format("YYYY-MM-DD") });
   }, [dueDate]);
+
+  useEffect(() => {
+    if (childData.premature_flag === 0) {
+      setChildData(prev => ({ ...prev, due_date: "" }));
+    } else if (childData.premature_flag === 1 && !childData.due_date) {
+      setChildData({ ...childData, due_date: dayjs(childData.due_date).format("YYYY-MM-DD") });
+      setDueDate(dayjs(birthDate).add(1, "day").toDate());
+    }
+  }, [childData.premature_flag]);
 
   const handleTypeInformation = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const id = evt.target.id;
@@ -89,33 +98,13 @@ const CreateChildPage = () => {
     }
   };
 
-  useEffect(() => {
-    if (childData.premature_flag === 0) {
-      setChildData(current => {
-        const { due_date, ...rest } = current;
-        return rest;
-      });
-    } else if (childData.premature_flag === 1 && !childData.due_date) {
-      setChildData({ ...childData, due_date: dayjs(childData.due_date).format("YYYY-MM-DD") });
-    }
-  }, [childData.premature_flag]);
-
   const handleSubmit = () => {
-    const validCheck = childrenList.find((child: any) => child.name === childData.name);
-
-    if (childrenList.length >= 5) {
-      setOpenLengthModal(true);
-      return;
+    if (isValid) {
+      createChild(childData);
+    } else {
+      setValidationModalTitle(validationModalTitleArr[titleNum]);
+      setOpenValidationModal(true);
     }
-    if (!childData.name) {
-      setOpenNameCheckModal(true);
-      return;
-    }
-    if (validCheck) {
-      setOpenSameNameCheckModal(true);
-      return;
-    }
-    createChild(childData);
   };
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, react/display-name
@@ -143,10 +132,10 @@ const CreateChildPage = () => {
           <S.InputTitle>아이 성별</S.InputTitle>
           <CustomRadioButton
             id="childGender"
-            type={Genders}
-            defaultValue={DEFAULT_GENDER}
+            options={GenderOption}
+            selectedValue={childData.gender}
             onChangeFunction={(e: React.ChangeEvent<HTMLInputElement>) => {
-              setChildData({ ...childData, gender: e.target.value });
+              setChildData({ ...childData, gender: e.target.value as "F" | "M" });
             }}
           />
 
@@ -169,13 +158,12 @@ const CreateChildPage = () => {
           <S.InputTitle>이른둥이 여부</S.InputTitle>
           <CustomRadioButton
             id="childPremeture"
-            type={Prematures}
-            defaultValue={DEFAULT_PREMATURE}
+            options={PrematureOption}
+            selectedValue={childData.premature_flag}
             onChangeFunction={(e: React.ChangeEvent<HTMLInputElement>) =>
               setChildData({ ...childData, premature_flag: Number(e.target.value) })
             }
           />
-
           {childData.premature_flag === 1 && (
             <>
               <S.InputTitle>기존 출산 예정일</S.InputTitle>
@@ -189,38 +177,54 @@ const CreateChildPage = () => {
                 dateFormat="yyyy.MM.dd"
                 showPopperArrow={false}
                 selected={dueDate}
-                minDate={birthDate}
+                minDate={dayjs(birthDate).add(1, "day").toDate()}
                 maxDate={dayjs(birthDate).add(90, "day").toDate()}
                 customInput={<CustomInput inputRef={inputRef} modifiable={true} />}
                 onChange={(date: Date | null) => setDueDate(date)}
               />
             </>
           )}
+          <S.InputTitle>제휴 기관</S.InputTitle>
+          {/* TODO: 제휴기관 등록 여부에 따라 다르게 보여주기 */}
+          <AffiliatedOrganizationBox handleClick={() => setOpenAffiliatedConfirmModal(true)} />
+          {/* <NoAffiliatedOrganizationBox /> */}
         </S.FormWrap>
-        <S.InputTitle>제휴 기관</S.InputTitle>
-        <AffiliatedOrganizationBox handleClick={() => console.log("d")} />
       </S.PageLayout>
-
-      <CustomModal
-        cancelBtn={false}
-        title="아이는 최대 5명 이상 등록할 수 없습니다."
-        isOpen={openLengthModal}
-        toggleModal={() => setOpenLengthModal(!openLengthModal)}
-        okBtnName="확인"
+      <ConfirmAffiliateOrganizationStatusModal
+        toggle={openAffiliatedConfirmModal}
+        handleToggle={() => setOpenAffiliatedConfirmModal(!openAffiliatedConfirmModal)}
+        handleDeleteBtnClick={async () => {
+          await setOpenAffiliatedConfirmModal(false);
+          await setOpenConfirmDeleteOrganizationModal(true);
+        }}
+        // TODO: 기관 변경하기 -> 진행중인 코칭 없을때 : 카메라 화면으로 이동
+        handleChangeBtnClick={async () => {
+          await setOpenAffiliatedConfirmModal(false);
+          await setOpenRejectChangeOrganizationModal(true);
+        }}
+      />
+      <ConfirmDeleteOrganizationModal
+        toggle={openConfirmDeleteOrganizationModal}
+        handleToggle={() => setOpenConfirmDeleteOrganizationModal(prev => !prev)}
+        handleDeleteBtnClick={async () => {
+          await setOpenConfirmDeleteOrganizationModal(false);
+          // TODO: 진행 중 코칭이 있을 시 다르게 로직 실행
+          await setOpenRejectDeleteOrganizationModal(true);
+        }}
+      />
+      <RejectDeleteOrganizationModal
+        toggle={openRejectDeleteOrganizationModal}
+        handleToggle={() => setOpenRejectDeleteOrganizationModal(prev => !prev)}
+      />
+      <RejectChangeOrganizationModal
+        toggle={openRejectChangeOrganizationModal}
+        handleToggle={() => setOpenRejectChangeOrganizationModal(prev => !prev)}
       />
       <CustomModal
         cancelBtn={false}
-        title="아이 이름을 입력해주세요."
-        isOpen={openNameCheckModal}
-        toggleModal={() => setOpenNameCheckModal(!openNameCheckModal)}
-        okBtnName="확인"
-      />
-
-      <CustomModal
-        cancelBtn={false}
-        title="같은 이름의 아이를 등록할 수 없습니다."
-        isOpen={openSameNameCheckModal}
-        toggleModal={() => setOpenSameNameCheckModal(!openSameNameCheckModal)}
+        title={validationModalTitle}
+        isOpen={openValidationModal}
+        toggleModal={() => setOpenValidationModal(!openValidationModal)}
         okBtnName="확인"
       />
       <CustomModal
