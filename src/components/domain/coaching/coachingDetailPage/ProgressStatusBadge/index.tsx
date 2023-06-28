a3361a90119c04d82928053c4668f18f46835bf1
ohcho-dev <ohcho.dev@gmail.com>
2023-06-28 18:46:48 +0900

    feat: create new badge for coaching

---
+++ src/components/domain/coaching/coachingDetailPage/ProgressStatusBadge/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./ProgressStatusBadge.styled";
+
+interface ProgressStatusBadgePropsType {
+  isFinished: boolean;
+}
+
+const ProgressStatusBadge = ({ isFinished = false }: ProgressStatusBadgePropsType) => {
+  return <S.BadgeWrapper isFinished>{isFinished ? "종료" : "진행중"}</S.BadgeWrapper>;
+};
+
+export default ProgressStatusBadge;
