6af229a3de9e14354de43dcb7e78e0ca687e6cba
ohcho-dev <ohcho.dev@gmail.com>
2023-06-29 17:19:50 +0900

    feat: add icon

--- src/components/domain/my/NoAffilatedOrganizationBox/index.tsx
+++ src/components/domain/my/NoAffilatedOrganizationBox/index.tsx
@@ -1,3 +1,5 @@
+import Icon from "components/common/Icon";
+import { ColorLightBlack8 } from "constants/ldsConstants/global";
 import * as S from "./NoAffiliatedOrganizationBox.styled";
 
 const NoAffiliatedOrganizationBox = () => {
@@ -7,6 +9,7 @@ const NoAffiliatedOrganizationBox = () => {
   return (
     <S.BoxWrapper onClick={handleSectionClick}>
       <S.InformText>아직 등록한 기관이 없습니다.</S.InformText>
+      <Icon icon={"home-plus"} size={24} fill={ColorLightBlack8} />
     </S.BoxWrapper>
   );
 };
