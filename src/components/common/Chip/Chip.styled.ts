b6df1e3d8b1b37a959212cc4cf36ecaa190d070c
ohcho-dev <ohcho.dev@gmail.com>
2023-06-30 13:38:33 +0900

    feat: add color code and refactor Chip component

--- src/components/common/Chip/Chip.styled.ts
+++ src/components/common/Chip/Chip.styled.ts
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 
-export const CustomChip = styled.div`
-  height: 2.2rem;
-  width: ${(prop: { customWidth: string | undefined }) => prop.customWidth || "4.3rem"};
-  color: ${prop => prop.color};
-  border: 1px solid ${prop => (prop.color === "#FFFFFF" ? "#282828" : prop.color)};
-  border-radius: 0.2rem;
-  background-color: ${prop => (prop.color === "#FFFFFF" ? "#282828" : "white")};
+export const CustomChip = styled.div<{
+  color: string;
+  borderColor: string;
+  backgroundColor: string;
+}>`
   display: inline-flex;
   align-items: center;
   justify-content: center;
+
+  height: 2.2rem;
+  width: auto;
+  padding: 0rem 0.6rem;
+  border-radius: 0.4rem;
+
+  color: ${({ color }) => color};
+  border: 1px solid ${({ borderColor }) => borderColor};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+
   font-size: 1.2rem;
   font-weight: 600;
+  line-height: 1.8rem;
 `;
--- src/components/common/Chip/index.tsx
+++ src/components/common/Chip/index.tsx
@@ -1,3 +1,19 @@
+import {
+  ColorDarkRed11,
+  ColorLight1,
+  ColorLightAmber10,
+  ColorLightAmber6,
+  ColorLightBrandBlue6,
+  ColorLightBrandBlue8,
+  ColorLightEltern3,
+  ColorLightEltern7,
+  ColorLightEltern9Base,
+  ColorLightPurple6,
+  ColorLightPurple8,
+  ColorLightRed6,
+  ColorLightSage9Base,
+  ColorLightSlate7,
+} from "constants/ldsConstants/global";
 import * as S from "./Chip.styled";
 
 interface chipProps {
@@ -6,23 +22,73 @@ interface chipProps {
 }
 
 const STATUS: {
-  [key: string]: { name: string; color: string; width?: string };
+  [key: string]: { name: string; color: string; borderColor: string; backgroundColor: string };
 } = {
-  TTPST_PENDING: { name: "대기중", color: "rgba(10,10,10,0.5)" },
-  TSTY_SURVEY: { name: "설문", color: "#FF8A00", width: "3.3rem" },
-  TSTY_VIDEO: { name: "동영상", color: "#8873F7" },
-  TSST_ONGOING: { name: "진행중", color: "rgba(0, 94, 236,0.7)" },
-  TSST_COMPLETE: { name: "완료", color: "rgba(92, 165, 37,0.9)", width: "3.3rem" },
-  TSST_UPLOAD: { name: "확인중", color: "#5AC4B1" },
-  TSST_REJECT: { name: "재등록요청", color: "#FF6B6B", width: "6.3rem" },
-  TTPST_COMPLETE: { name: "발급완료", color: "#FFFFFF", width: "5.3rem" },
-  EXPIRED: { name: "기한만료", color: "#FFFFFF", width: "5.3rem" },
+  TTPST_PENDING: {
+    name: "대기중",
+    color: ColorLightSage9Base,
+    borderColor: ColorLightSlate7,
+    backgroundColor: ColorLight1,
+  },
+  TSTY_SURVEY: {
+    name: "설문",
+    color: ColorLightAmber10,
+    borderColor: ColorLightAmber6,
+    backgroundColor: ColorLight1,
+  },
+  TSTY_VIDEO: {
+    name: "동영상",
+    color: ColorLightPurple8,
+    borderColor: ColorLightPurple6,
+    backgroundColor: ColorLight1,
+  },
+  TSST_ONGOING: {
+    name: "진행중",
+    color: ColorLightBrandBlue8,
+    borderColor: ColorLightBrandBlue6,
+    backgroundColor: ColorLight1,
+  },
+  TSST_COMPLETE: {
+    name: "완료",
+    color: ColorLightEltern9Base,
+    borderColor: ColorLightEltern7,
+    backgroundColor: ColorLightEltern3,
+  },
+  TSST_UPLOAD: {
+    name: "확인중",
+    color: ColorLightEltern9Base,
+    borderColor: ColorLightEltern7,
+    backgroundColor: ColorLight1,
+  },
+  TSST_REJECT: {
+    name: "재등록요청",
+    color: ColorDarkRed11,
+    borderColor: ColorLightRed6,
+    backgroundColor: ColorLight1,
+  },
+  TTPST_COMPLETE: {
+    name: "발급완료",
+    color: ColorLightEltern9Base,
+    borderColor: ColorLightEltern7,
+    backgroundColor: ColorLightEltern3,
+  },
+  EXPIRED: {
+    name: "기한만료",
+    color: "#FFFFFF",
+    borderColor: ColorLight1,
+    backgroundColor: ColorLight1,
+  },
 };
 
 const Chip = (props: chipProps) => {
   const { status, style } = props;
   return (
-    <S.CustomChip color={STATUS[status].color} customWidth={STATUS[status].width} style={style}>
+    <S.CustomChip
+      color={STATUS[status].color}
+      borderColor={STATUS[status].borderColor}
+      backgroundColor={STATUS[status].backgroundColor}
+      style={style}
+    >
       {STATUS[status].name}
     </S.CustomChip>
   );
