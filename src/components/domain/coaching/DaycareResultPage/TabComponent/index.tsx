847cee39ab73bc60cf2ee86b58353e8c1625ee06
ohcho-dev <ohcho.dev@gmail.com>
2023-07-06 16:47:22 +0900

    feat: modify tab component menu order

--- src/components/domain/coaching/DaycareResultPage/TabComponent/index.tsx
+++ src/components/domain/coaching/DaycareResultPage/TabComponent/index.tsx
@@ -2,32 +2,31 @@ import { CategoryListType } from "types/apis/coaching";
 import * as S from "./TabComponent.styled";
 
 interface TabComponentPropsType {
-  list: CategoryListType[] | undefined;
   selectedCategory: number;
   handleCategoryClick: (id: number) => void;
 }
 
-const TabComponent = ({ list, selectedCategory, handleCategoryClick }: TabComponentPropsType) => {
+const TAB_MENU = [
+  { name: "종합", id: 0 },
+  { name: "대근육", id: 1 },
+  { name: "소근육", id: 2 },
+  { name: "언어", id: 5 },
+  { name: "인지", id: 4 },
+  { name: "사회성", id: 3 },
+];
+
+const TabComponent = ({ selectedCategory, handleCategoryClick }: TabComponentPropsType) => {
   return (
     <S.TabWrapper>
-      <S.TabItem selected={selectedCategory === 0} key={0} onClick={() => handleCategoryClick(0)}>
-        <S.TabTitle selected={selectedCategory === 0}>종합</S.TabTitle>
-      </S.TabItem>
-      {list &&
-        list.map(item => (
-          <S.TabItem
-            selected={selectedCategory === item.growth_category_id}
-            key={item.growth_category_id}
-            onClick={() => handleCategoryClick(item.growth_category_id)}
-          >
-            <S.TabTitle selected={selectedCategory === item.growth_category_id}>
-              {item.growth_category_name}
-            </S.TabTitle>
-          </S.TabItem>
-        ))}
-      <S.TabItem selected={selectedCategory === 5} key={5} onClick={() => handleCategoryClick(5)}>
-        <S.TabTitle selected={selectedCategory === 5}>언어</S.TabTitle>
-      </S.TabItem>
+      {TAB_MENU.map(({ name, id }) => (
+        <S.TabItem
+          selected={selectedCategory === id}
+          key={id}
+          onClick={() => handleCategoryClick(id)}
+        >
+          <S.TabTitle selected={selectedCategory === id}>{name}</S.TabTitle>
+        </S.TabItem>
+      ))}
     </S.TabWrapper>
   );
 };
