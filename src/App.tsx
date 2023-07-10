17d0decc17d1d447a600dd72179112f995acfb39
ohcho-dev <ohcho.dev@gmail.com>
2023-07-10 16:31:22 +0900

    feat: change logic when push message is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,24 @@ import "./styles/_global.scss";
 import "./styles/_slideTransition.scss";
 import "./styles/_customReactDatepicker.scss";
 
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { RouterConfig } from "./RouteConfig";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 import { CHILD_ID_FIELD, USER_KEY } from "./constants/localStorage";
 import { ErrorBoundary } from "./pages/common/ErrorPage/ErrorPage";
 import { NativeFunction } from "./utils/app/NativeFunction";
-import { childrenKeyState, childrenListState, selectedChildInfoState } from "./store/common";
+import {
+  childrenKeyState,
+  childrenListState,
+  newNotificationFlagstate,
+  selectedChildInfoState,
+} from "./store/common";
 import { currentTaskIdState } from "./store/domain/coaching";
 import { commonQueryKeys } from "./queries/common/commonQueryKeys";
 import { coachingQueryKeys } from "./queries/domain/coaching/coachingQueryKeys";
 import { myQueryKeys } from "./queries/domain/my/myQueryKeys";
 import useGetBaseDate from "hooks/useGetBaseData";
+import useUpdateNotificationCheckTime from "queries/common/notification/useUpdateNotificationCheckTime";
 
 let oldLocation: any = null;
 
@@ -37,8 +43,8 @@ const App: React.FC = () => {
   const childrenList = useRecoilValue(childrenListState);
   const setChildrenKey = useSetRecoilState(childrenKeyState);
   const currentTaskId = useRecoilValue(currentTaskIdState);
-  const [resultId, setResultId] = useState("");
-  const [videoId, setVideoId] = useState("");
+  const setNewNotificationFlag = useSetRecoilState(newNotificationFlagstate);
+  const { mutate: updateNotificationTime } = useUpdateNotificationCheckTime();
   const data = useGetBaseDate();
 
   function refetchData() {
@@ -91,12 +97,13 @@ const App: React.FC = () => {
     });
 
     window.addEventListener("coachingResult", (res: any) => {
-      setResultId(res.detail.id);
+      navigate(`/coaching/result/${res.detail.id}`);
     });
 
-    window.addEventListener("coachingVideoAssignment", (res: any) => {
-      navigate(`/coaching/videoAssignment/${res.detail.id}`);
-      // setVideoId(res.detail.id);
+    window.addEventListener("coachingVideoAssignment", async (res: any) => {
+      await setNewNotificationFlag(false);
+      await updateNotificationTime();
+      await navigate(`/coaching/videoAssignment/${res.detail.id}`);
     });
   }, []);
 
@@ -112,15 +119,6 @@ const App: React.FC = () => {
       }
     }
   }, [window.localStorage.getItem(USER_KEY), selectedChild]);
-
-  useEffect(() => {
-    if (resultId) {
-      navigate(`/coaching/result/${resultId}`);
-    }
-    // if (videoId) {
-    //   navigate(`/coaching/videoAssignment/${videoId}`);
-    // }
-  }, [resultId]);
 
   useEffect(() => {
     if (childrenList.length) {
