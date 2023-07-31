ea62ea4236f9dde7ce5adb94bc06733b43ca2b37
ohcho-dev <ohcho.dev@gmail.com>
2023-07-31 15:09:39 +0900

    feat: add props for EmptyBox component

--- src/components/common/EmptyBox/index.tsx
+++ src/components/common/EmptyBox/index.tsx
@@ -2,10 +2,11 @@ import * as S from "./EmptyBox.styled";
 
 interface EmptyBoxPropsType {
   height?: string;
+  backgroundColor?: string;
 }
 
-const EmptyBox = ({ height }: EmptyBoxPropsType): JSX.Element => {
-  return <S.Empty style={height ? { height } : {}} />;
+const EmptyBox = ({ height, backgroundColor }: EmptyBoxPropsType): JSX.Element => {
+  return <S.Empty style={{ height: height, backgroundColor: backgroundColor }} />;
 };
 
 export default EmptyBox;
