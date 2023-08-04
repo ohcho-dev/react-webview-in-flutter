3c2d1cceadd9e3dab5925a401496ddaa7036d71b
ohcho-dev <ohcho.dev@gmail.com>
2023-08-04 14:26:09 +0900

    feat: create SpeechBubble component

--- src/components/domain/coaching/qnaPage/QnaStatusBox/QnaStatusBox.styled.ts
+++ src/components/domain/coaching/qnaPage/QnaStatusBox/QnaStatusBox.styled.ts
@@ -1,5 +1,18 @@
-import { ColorLightSlate2, ColorLightSlate6 } from "lds-common/src/constants/tokens/global";
+import {
+  ColorLightBlack9Base,
+  ColorLightSlate2,
+  ColorLightSlate6,
+} from "lds-common/src/constants/tokens/global";
 import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+`;
+
+export const SpeechBubbleWrapper = styled.div`
+  position: absolute;
+  bottom: -4rem;
+`;
 
 export const QnaStatusBox = styled.div`
   display: grid;
@@ -22,4 +35,25 @@ export const QnaStatusBoxItem = styled.div<{ middleOne?: boolean }>`
 
   border-left: ${({ middleOne }) => middleOne && `1px solid ${ColorLightSlate6}`};
   border-right: ${({ middleOne }) => middleOne && `1px solid ${ColorLightSlate6}`};
+`;
+
+export const SpeechBubble = styled.div`
+  width: fit-content;
+  min-width: 10rem;
+  position: relative;
+  border-radius: 0.6rem;
+  background-color: ${ColorLightBlack9Base};
+
+  padding: 0.8rem 1rem;
+
+  :before {
+    border-top: 0px solid transparent;
+    border-left: 0.8rem solid transparent;
+    border-right: 0.8rem solid transparent;
+    border-bottom: 0.8rem solid ${ColorLightBlack9Base};
+    content: "";
+    position: absolute;
+    top: -0.6rem;
+    left: 5.3rem;
+  }
 `;
--- src/components/domain/coaching/qnaPage/QnaStatusBox/index.tsx
+++ src/components/domain/coaching/qnaPage/QnaStatusBox/index.tsx
@@ -1,9 +1,11 @@
 import {
   ColorLightBlack7,
+  ColorLightSlate6,
   ColorLightSlate8,
   ColorLightSlate9Base,
   TextLg1826Semibold,
   TextSm1420Regular,
+  TextXs1218Medium,
 } from "lds-common/src/constants/tokens/global";
 import * as S from "./QnaStatusBox.styled";
 import Text from "components/common/Text";
@@ -16,41 +18,50 @@ interface QnaStatusBoxProps {
 
 const QnaStatusBox = ({ onGoingQnaNum, questionNum, responseCompletedNum }: QnaStatusBoxProps) => {
   return (
-    <S.QnaStatusBox>
-      <S.QnaStatusBoxItem>
-        <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
-          질문권
-        </Text>
-        <Text
-          variant={TextLg1826Semibold}
-          color={questionNum ? ColorLightBlack7 : ColorLightSlate8}
-        >
-          {`${questionNum}`}
-        </Text>
-      </S.QnaStatusBoxItem>
-      <S.QnaStatusBoxItem middleOne>
-        <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
-          진행중
-        </Text>
-        <Text
-          variant={TextLg1826Semibold}
-          color={onGoingQnaNum ? ColorLightBlack7 : ColorLightSlate8}
-        >
-          {`${onGoingQnaNum}`}
-        </Text>
-      </S.QnaStatusBoxItem>
-      <S.QnaStatusBoxItem>
-        <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
-          답변완료
-        </Text>
-        <Text
-          variant={TextLg1826Semibold}
-          color={responseCompletedNum ? ColorLightBlack7 : ColorLightSlate8}
-        >
-          {`${responseCompletedNum}`}
-        </Text>
-      </S.QnaStatusBoxItem>
-    </S.QnaStatusBox>
+    <S.Wrapper>
+      <S.QnaStatusBox>
+        <S.QnaStatusBoxItem>
+          <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
+            질문권
+          </Text>
+          <Text
+            variant={TextLg1826Semibold}
+            color={questionNum ? ColorLightBlack7 : ColorLightSlate8}
+          >
+            {`${questionNum}`}
+          </Text>
+        </S.QnaStatusBoxItem>
+        <S.QnaStatusBoxItem middleOne>
+          <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
+            진행중
+          </Text>
+          <Text
+            variant={TextLg1826Semibold}
+            color={onGoingQnaNum ? ColorLightBlack7 : ColorLightSlate8}
+          >
+            {`${onGoingQnaNum}`}
+          </Text>
+        </S.QnaStatusBoxItem>
+        <S.QnaStatusBoxItem>
+          <Text variant={TextSm1420Regular} color={ColorLightSlate9Base}>
+            답변완료
+          </Text>
+          <Text
+            variant={TextLg1826Semibold}
+            color={responseCompletedNum ? ColorLightBlack7 : ColorLightSlate8}
+          >
+            {`${responseCompletedNum}`}
+          </Text>
+        </S.QnaStatusBoxItem>
+      </S.QnaStatusBox>
+      <S.SpeechBubbleWrapper>
+        <S.SpeechBubble>
+          <Text variant={TextXs1218Medium} color={ColorLightSlate6}>
+            2023.06.30일까지만 질문권을 이용하실 수 있어요.
+          </Text>
+        </S.SpeechBubble>
+      </S.SpeechBubbleWrapper>
+    </S.Wrapper>
   );
 };
 
