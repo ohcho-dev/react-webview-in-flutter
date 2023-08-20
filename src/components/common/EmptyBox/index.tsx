3603a02d57fcde6ea9aa3c0c71ff04d62a92ac5a
ohcho-dev <ohcho.dev@gmail.com>
2023-08-21 08:59:33 +0900

    style: remove default backgroundColor for EmptyBox

--- src/components/common/EmptyBox/index.tsx
+++ src/components/common/EmptyBox/index.tsx
@@ -1,4 +1,3 @@
-import { ColorLightSlate2 } from "lds-common/src/constants/tokens/global";
 import * as S from "./EmptyBox.styled";
 
 interface EmptyBoxPropsType {
@@ -6,10 +5,7 @@ interface EmptyBoxPropsType {
   backgroundColor?: string;
 }
 
-const EmptyBox = ({
-  height = "1rem",
-  backgroundColor = ColorLightSlate2,
-}: EmptyBoxPropsType): JSX.Element => {
+const EmptyBox = ({ height = "1rem", backgroundColor }: EmptyBoxPropsType): JSX.Element => {
   return <S.Empty style={{ height: height, backgroundColor: backgroundColor }} />;
 };
 
